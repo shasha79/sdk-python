19d9d4eff9214a9cc6eabc04bf6c7be7fbed14ad
Vadim <31348740+vchulski@users.noreply.github.com>
2020-04-01 01:21:11 +0300

    update get_item method
    Added optional `fields` parameter to indicate only needed fields to extract.

--- directus_api/directus.py
+++ directus_api/directus.py
@@ -23,12 +23,48 @@ class DirectusClient:
             self.access = data['data']["token"]
 
     def get_items_list(self, collection, limit=200):
+        """Function list items from collection. 
+        
+        Parameters
+        ----------
+        collection : str
+            collection name
+        limit : int
+            A limit on the number of objects that are returned. Default is 200.
+            To grab all items from collection pass: `-1`, but be careful this could take a lot of time.
+            
+                    
+        Original method description:
+        https://docs.directus.io/api/items.html#list-the-items
+        """
         headers = {"Authorization": "Bearer " + self.access}
         return requests.get(self.url + "/items/" + collection + "?limit=" + str(limit), headers=headers).json()
 
-    def get_item(self, collection, id):
+    def get_item(self, collection, id, fields="*"):
+        """Function get item from collection. 
+        
+        Parameters
+        ----------
+        collection : str
+            collection name
+        id : str or int
+            id of the item
+        fields : str, optional
+            Special sign to select only needed fields. 
+            Popular options are: 
+            `*` - default. Get all top-level fields. 
+            `*.*` - Get all top-level fields and all second-level relational fields.
+            `*,images.*` - Get all top-level fields and second-level relational fields within images
+            
+            All signs could be founded here: 
+            https://docs.directus.io/api/query/fields.html
+            
+        Original method description:
+        https://docs.directus.io/api/items.html#retrieve-an-item
+        """
+        
         headers = {"Authorization": "Bearer " + self.access}
-        return requests.get(self.url + "/items/" + collection + "/" + str(id), headers=headers).json()
+        return requests.get(self.url + "/items/" + collection + "/" + str(id)+"?fields="+str(fields), headers=headers).json()
 
     def create_item(self, collection, item):
         headers = {"Authorization": "Bearer " + self.access}
